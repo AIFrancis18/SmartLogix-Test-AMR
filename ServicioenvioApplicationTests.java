95b72b4e7b3eaf5c12b6d2f61abec65e259c9814
AIFrancis18 <al.francis@duocuc.cl>
2026-06-04 16:08:33 -0400

    Agrega pruebas unitarias de EnvioServiceImplTest

---
+++ ServicioenvioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartlogix.servicioenvio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioenvioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
