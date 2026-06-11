c559a092d257c49bfdfc688c2d6dcbf65ad51cd6
MaxiMillacaris <ma.millacaris@duocuc.cl>
2026-06-11 17:54:21 -0400

    Subi los test del microservicio notificaciones

---
+++ ServicionotificacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartlogix.servicionotificacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicionotificacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
